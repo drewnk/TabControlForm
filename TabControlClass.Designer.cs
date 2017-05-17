56fb801cb117e27952c5ccecec33fd0ac5f88de2
Drew Nagy-Kato <dnagykato@gmail.com>
2017-05-16 22:49:19 -0400

    Initial commit.

---
+++ TabControlClass.Designer.cs
@@ -0,0 +1,131 @@
+﻿namespace TabControlForm
+{
+    partial class TabControlClass
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox = new System.Windows.Forms.TextBox();
+            this.addButton = new System.Windows.Forms.Button();
+            this.deleteButton = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.tabPage1 = new System.Windows.Forms.TabPage();
+            this.tabPage2 = new System.Windows.Forms.TabPage();
+            this.tabControl.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // textBox
+            // 
+            this.textBox.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.textBox.Location = new System.Drawing.Point(13, 315);
+            this.textBox.Name = "textBox";
+            this.textBox.Size = new System.Drawing.Size(127, 20);
+            this.textBox.TabIndex = 3;
+            this.textBox.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.textBox_KeyPress);
+            // 
+            // addButton
+            // 
+            this.addButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Left)));
+            this.addButton.Location = new System.Drawing.Point(147, 312);
+            this.addButton.Name = "addButton";
+            this.addButton.Size = new System.Drawing.Size(56, 23);
+            this.addButton.TabIndex = 4;
+            this.addButton.Text = "Add Tab";
+            this.addButton.UseVisualStyleBackColor = true;
+            this.addButton.Click += new System.EventHandler(this.addButton_Click);
+            // 
+            // deleteButton
+            // 
+            this.deleteButton.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.deleteButton.Location = new System.Drawing.Point(404, 312);
+            this.deleteButton.Name = "deleteButton";
+            this.deleteButton.Size = new System.Drawing.Size(77, 23);
+            this.deleteButton.TabIndex = 5;
+            this.deleteButton.Text = "Remove Tab";
+            this.deleteButton.UseVisualStyleBackColor = true;
+            this.deleteButton.Click += new System.EventHandler(this.deleteButton_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Controls.Add(this.tabPage1);
+            this.tabControl.Controls.Add(this.tabPage2);
+            this.tabControl.Location = new System.Drawing.Point(13, 13);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(468, 293);
+            this.tabControl.TabIndex = 0;
+            // 
+            // tabPage1
+            // 
+            this.tabPage1.Location = new System.Drawing.Point(4, 22);
+            this.tabPage1.Name = "Tab 1";
+            this.tabPage1.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage1.Size = new System.Drawing.Size(460, 267);
+            this.tabPage1.TabIndex = 1;
+            this.tabPage1.Text = "Tab 1";
+            this.tabPage1.UseVisualStyleBackColor = true;
+            // 
+            // tabPage2
+            // 
+            this.tabPage2.Location = new System.Drawing.Point(4, 22);
+            this.tabPage2.Name = "Tab 2";
+            this.tabPage2.Padding = new System.Windows.Forms.Padding(3);
+            this.tabPage2.Size = new System.Drawing.Size(522, 327);
+            this.tabPage2.TabIndex = 2;
+            this.tabPage2.Text = "Tab 2";
+            this.tabPage2.UseVisualStyleBackColor = true;
+            // 
+            // TabControlClass
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(493, 347);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.deleteButton);
+            this.Controls.Add(this.addButton);
+            this.Controls.Add(this.textBox);
+            this.Name = "TabControlClass";
+            this.Text = "TabControlClass";
+            this.tabControl.ResumeLayout(false);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox;
+        private System.Windows.Forms.Button addButton;
+        private System.Windows.Forms.Button deleteButton;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.TabPage tabPage1;
+        private System.Windows.Forms.TabPage tabPage2;
+    }
+}
+
---
+++ TabControlClass.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace TabControlForm
+{
+    public partial class TabControlClass : Form
+    {
+        public TabControlClass()
+        {
+            InitializeComponent();
+        }
+
+        private void TabControl_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        // Add tabPage, if it doesn't exist; otherwise, select it
+        private void addButton_Click(object sender, EventArgs e)
+        {
+            addNewTab();
+        }
+
+        // Add tabPage when user presses Enter from textBox
+        private void textBox_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            if(e.KeyChar == (char)Keys.Enter)
+                addNewTab();
+        }
+
+        // Remove tabPage from tabControl
+        private void deleteButton_Click(object sender, EventArgs e)
+        {
+            if(tabControl.SelectedTab != null)
+            {
+                tabControl.TabPages.Remove(tabControl.SelectedTab);
+                tabControl.SelectedIndex = tabControl.TabPages.Count - 1;
+            }
+        }
+
+        // Check whether a tabPage already exists
+        private int? SearchIndex(String text)
+        {
+            if (tabControl == null)
+                return null;
+
+            foreach (TabPage tab in tabControl.TabPages)
+                if (tab.Text == text)
+                    return tabControl.TabPages.IndexOf(tab);
+
+            return null;
+        }
+
+        // Generate new TabPage
+        private void addNewTab()
+        {
+            String text = textBox.Text.Trim();
+
+            if (text.Length != 0)
+            {
+                int? index = SearchIndex(text);
+
+                if (index == null)
+                {
+                    TabPage newTab = new System.Windows.Forms.TabPage();
+
+                    newTab.Location = new System.Drawing.Point(4, 22);
+                    newTab.Name = text;
+                    newTab.Padding = new System.Windows.Forms.Padding(3);
+                    newTab.Size = new System.Drawing.Size(460, 267);
+                    newTab.TabIndex = tabControl.TabPages.Count;
+                    newTab.Text = text;
+                    newTab.UseVisualStyleBackColor = true;
+
+                    tabControl.TabPages.Add(newTab);
+                    tabControl.SelectedIndex = tabControl.TabPages.IndexOf(newTab);
+                }
+                else
+                    tabControl.SelectedIndex = Convert.ToInt32(index);
+            }
+            else
+                textBox.Clear();
+        }
+    }
+}
